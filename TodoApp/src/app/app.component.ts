f18c3ef2bc12cb6529367d0e820ff7161cc80aa8
Chelsea Gomes <chelsealizygomes11@gmail.com>
2025-03-12 15:06:40 +0530

    feat(app): create todos architecture
    - Navbar & Footer Components: Created sample navbar and footer components.
    - Routing Setup:
    1) Configured routing for Todo List and Todo Detail components.
    2) Implemented routerLink in the Todo List component to navigate to the Todo Detail component.
    3) Used routerOutlet to display the Todo List component.
    - Data & Type Safety:
    1)Added mock data for testing.
    2)Created an interface and applied types to appropriate places.
    - Service for API Calls:
    Built a service to test the GET request for fetching todos.
    - Project Structure:
    * Organized the project by creating folders:
    1) models (for interfaces)
    components
    2) pages
    3) services
    4) shared (for reusable elements)
    API Configuration:
    5) Used provideHttpClient(withFetch()) for making API calls with HttpClient.

--- TodoApp/src/app/app.component.ts
+++ TodoApp/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'TodoApp';
