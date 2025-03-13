4bf0473508948891b738c51fd92e16224c4bf3b8
Chelsea Gomes <chelsealizygomes11@gmail.com>
2025-03-13 19:04:52 +0530

    feat(app): add crud operations for REST API calls
    - ellipsis on mobile screens
    - add media query for desktop
    - add update functionality in UI
    - add post functionality in UI with input field using NgModel
    - add delete functionality in UI
    - remove routerlink for timebeing

--- TodoApp/src/app/services/todo.service.ts
+++ TodoApp/src/app/services/todo.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
-import { AllTodo } from '../models/todo.model';
+import { AllTodo, Todo } from '../models/todo.model';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +12,31 @@ export class TodoService {
 
   private http = inject(HttpClient);
 
+  //read all
   getAllTodos(): Observable<AllTodo> {
     return this.http.get<AllTodo>(this.apiUrl);
+  }
+
+  //read by id
+  getTodoById(id: number): Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.apiUrl}/${id}`);
+  }
+
+  //update
+  editTodo(updatedTodo: Todo): Observable<Todo> {
+    return this.http.patch<Todo>(`${this.apiUrl}/${updatedTodo.id}`, {
+      todo: updatedTodo.todo,
+    });
+  }
+
+  //create
+  addTodo(newTodo: Todo): Observable<Todo> {
+    const url = `${this.apiUrl}/add`;
+    return this.http.post<Todo>(url, newTodo);
+  }
+
+  //delete
+  deleteTodo(id: number): Observable<Todo> {
+    return this.http.delete<Todo>(`${this.apiUrl}/${id}`);
   }
 }
